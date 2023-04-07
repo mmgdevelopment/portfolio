0514596793a06b56bf9eae2bb387fbe50ebd2cd8
Marcel Gregor <marcel_gregor@icloud.com>
2023-04-07 19:21:06 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ImprintComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
